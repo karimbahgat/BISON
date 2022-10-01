b9a0952d51b63f1952fc2f782e65a10966ebb4aa
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-10-01 18:31:31 +0200

    Show similar geoms in table in a nicer way

--- adminGeocoder/static/adminGeocoder/js/disambiguator.js
+++ adminGeocoder/static/adminGeocoder/js/disambiguator.js
@@ -28,8 +28,6 @@ function initDisambiguator(searchId2) {
     currentSelectedGeomId = data.chosen_geom_id
     // show currently selected geom on map
     requestGeomForMap(currentSelectedGeomId);
-    // also show all similar geoms to map
-    requestSimilarGeomsForMap(currentSelectedGeomId);
     // add all possible geom candidates to table
     for (result of data.results) {
         for (adminId of result.admins) {
@@ -37,6 +35,8 @@ function initDisambiguator(searchId2) {
             requestGeomCandidate(adminId);
         };
     };
+    // also show all similar geoms to map
+    requestSimilarGeomsForMap(currentSelectedGeomId);
 }
 
 function requestGeomForMap(adminId) {
@@ -50,26 +50,15 @@ function receiveGeomForMap(data) {
     addGeomToDisambiguationMap(data);
 }
 
-function requestSimilarGeomsForMap(adminId) {
-    // fetch full details of geom
-    url = '/api/get_similar_admins/' + adminId;
-    fetch(url).then(result=>result.json()).then(data=>receiveSimilarGeomsForMap(data));
-}
-
-function receiveSimilarGeomsForMap(data) {
-    for (entry of data.results) {
-        requestGeomForMap(entry.id);
-    };
-}
-
 function addGeomToDisambiguationTable(adminId, result) {
     tbody = document.querySelector('#disambiguation-geom-table tbody');
     tr = document.createElement('tr');
     tr.id = 'admin-candidate-id-' + adminId;
+    tr.className = 'admin-candidate-row';
     tr.onclick = function(){selectGeom(adminId)};
     tr.innerHTML = `
     <td>...</td>
-    <td>${(result.simil * 100).toFixed(1)}%</td>
+    <td class="admin-name-match-percent"><img src="static/images/text-icon.png"><span>${(result.simil * 100).toFixed(1)}%</span></td>
     <td>...</td>
     <td>...</td>
     `;
@@ -105,7 +94,7 @@ function updateDisambiguationTableEntry(geomData) {
     tdList[3].innerText = validity;
     // mark as selected
     if (geomData.id == currentSelectedGeomId) {
-        tr.className = "selected-geom-row";
+        tr.className = "admin-candidate-row selected-geom-row";
         tr.scrollIntoView({block:'nearest', inline:'nearest'});
     };
 }
@@ -122,13 +111,13 @@ function updateLoadStatus() {
 
 function selectGeom(adminId) {
     // mark the table entry as selected
-    rows = document.querySelectorAll('#disambiguation-geom-table tbody tr');
+    rows = document.querySelectorAll('#disambiguation-geom-table tbody .admin-candidate-row');
     for (tr of rows) {
         if (tr.id == `admin-candidate-id-${adminId}`) {
-            tr.className = "selected-geom-row";
+            tr.className = "admin-candidate-row selected-geom-row";
             tr.scrollIntoView({block:'nearest', inline:'nearest'});
         } else {
-            tr.className = "";
+            tr.className = "admin-candidate-row";
         };
     };
     // clear map
@@ -164,4 +153,77 @@ function cancelDisambiguator() {
     currentSelectedGeomId = searchData.chosen_geom_id;
     // close popup
     document.getElementById('disambiguation-popup').className = 'popup is-hidden';
+}
+
+///////////////////
+// similar geoms
+
+function requestSimilarGeomsForMap(adminId) {
+    // clear any old similar geoms info
+    clearSimilarGeomsFromTable();
+    // indicate loading to the user
+    showSimilarGeomsLoading(adminId);
+    // fetch full details of geom
+    url = '/api/get_similar_admins/' + adminId;
+    fetch(url).then(result=>result.json()).then(data=>receiveSimilarGeomsForMap(data));
+}
+
+function clearSimilarGeomsFromTable() {
+    elem = document.querySelector('.similar-geoms-tr');
+    if (elem) {elem.remove()};
+}
+
+function showSimilarGeomsLoading(adminId) {
+    // indicate loading by adding a row with loading text
+    // right below the table row of this adminId
+    tr = document.getElementById('admin-candidate-id-' + adminId);
+    similar_tr = document.createElement('tr');
+    similar_tr.className = 'similar-geoms-tr';
+    similar_td = document.createElement('td');
+    similar_td.className = 'similar-geoms-td';
+    similar_td.colSpan = "4";
+    similar_tr.appendChild(similar_td);
+    tr.parentNode.insertBefore(similar_tr, tr.nextSibling);
+
+    // create a text to indicate loading
+    span = document.createElement('span');
+    span.className = 'similar-geoms-loading';
+    span.innerHTML = '<img src="static/images/Spinner-1s-200px.gif"> Looking for similar geometries';
+    similar_td.appendChild(span);
+
+    // create a table inside this tr
+    table = document.createElement('table');
+    table.className = 'similar-geoms-table';
+    similar_td.appendChild(table);
+}
+
+function receiveSimilarGeomsForMap(data) {
+    // remove loading text
+    span = document.querySelector('.similar-geoms-loading');
+    span.remove();
+    // add similar geoms
+    for (entry of data.results) {
+        addSimilarGeomToTable(entry);
+        requestGeomForMap(entry.id);
+    };
+}
+
+function addSimilarGeomToTable(entry) {
+    console.log(entry)
+    table = document.querySelector('.similar-geoms-table');
+    tr = document.createElement('tr');
+    tr.id = 'similar-geom-id-' + entry.id;
+    tr.className = 'similar-geom-admin';
+    if (entry.valid_from === null) {
+        validity = 'Unknown';
+    } else {
+        validity = `${entry.valid_from} - ${entry.valid_to}`;
+    };
+    tr.innerHTML = `
+    <td>&#9654;${getDisplayName(entry)}</td>
+    <td class="similar-geom-match-percent"><img src="static/images/square.png"><span>${(entry.simil * 100).toFixed(1)}%</span></td>
+    <td>${entry.source.name}</td>
+    <td>${validity}</td>
+    `;
+    table.appendChild(tr);
 }
