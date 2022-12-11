a9e2de4ffed2a6cf491cbf1ebc351c72841ff826
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-12-12 00:18:41 +0100

    Fix dataset/importer edit

--- adminManager/views.py
+++ adminManager/views.py
@@ -38,12 +38,13 @@ def datasource(request, pk):
 
 def datasource_add(request):
     if request.method == 'GET':
+        # TODO: shouldnt happen, since dataset add is a popup, not a page
         jkllkjljljljlj
 
         # create empty form
-        form = forms.AdminSourceForm(initial={'type':'DataSource'})
-        context = {'form': form}
-        return render(request, 'adminManager/source_data_add.html', context)
+        #form = forms.AdminSourceForm(initial={'type':'DataSource'})
+        #context = {'form': form}
+        #return render(request, 'adminManager/source_data_add.html', context)
 
     elif request.method == 'POST':
         with transaction.atomic():
@@ -71,6 +72,7 @@ def datasource_add(request):
                     raise NotImplementedError('Invalid form handling needs to be added by redirecting to sources.html with popup')
 
                 return redirect('dataset', source.pk)
+
             else:
                 raise NotImplementedError('Invalid form handling needs to be added by redirecting to sources.html with popup')
                 return render(request, 'adminManager/source_data_add.html', {'form':form})
@@ -80,24 +82,38 @@ def datasource_edit(request, pk):
     src = models.AdminSource.objects.get(pk=pk)
 
     if request.method == 'GET':
-        # create empty form
+        # create editable source form
         form = forms.AdminSourceForm(instance=src)
-        import_params_form = DatasetImporterForm(instance=src.importer)
-        context = {'form': form, 'import_params_form': import_params_form}
+        # create editable import forms
+        DatasetImporterFormset = modelformset_factory(DatasetImporter, 
+                                                    form=DatasetImporterForm,
+                                                    extra=0)
+        queryset = DatasetImporter.objects.filter(source=src)
+        importer_forms = DatasetImporterFormset(queryset=queryset)
+        context = {'form': form, 'importer_forms': importer_forms}
         return render(request, 'adminManager/source_data_edit.html', context)
 
     elif request.method == 'POST':
         with transaction.atomic():
             # save form data
             data = request.POST
+            print(data)
             form = forms.AdminSourceForm(data, instance=src)
             if form.is_valid():
                 form.save()
-                # save importer
-                importer = src.importer
-                importer.import_params = json.loads(data['import_params'])
-                importer.save()
-                return redirect('source', src.pk)
+                # save importers
+                DatasetImporterFormset = modelformset_factory(DatasetImporter, 
+                                                            form=DatasetImporterForm,
+                                                            extra=0)
+                importer_forms = DatasetImporterFormset(data)
+                if importer_forms.is_valid():
+                    importer_forms.save()
+                else:
+                    print(importer_forms.errors)
+                    return render(request, 'adminManager/source_data_edit.html', {'form':form, 'importer_forms':importer_forms})
+
+                return redirect('dataset', src.pk)
+
             else:
-                return render(request, 'adminManager/source_data_edit.html', {'form':form})
+                return render(request, 'adminManager/source_data_edit.html', {'form':form, 'importer_forms':importer_forms})
 
