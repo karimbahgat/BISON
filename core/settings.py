06b5c1d5eb6a1c38531d6aca3f0affe88918f4a9
Jenn Wheeler <jawheeler@wm.edu>
2022-12-14 13:44:00 -0500

    pymysql compatibility with Django

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
