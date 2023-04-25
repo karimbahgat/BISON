e4de36bae977f42aa4c6b4d154d237abf99d2b56
Karim Bahgat <karim.bahgat.norway@gmail.com>
2023-04-25 12:56:04 +0200

    Try to fix background tasks not running

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py loaddata AdminSource.json DatasetImporter.json
-start_background_tasks.sh & echo "background task runner started"
-gunicorn -w 2 -b 0.0.0.0:8000 core.wsgi --timeout 300
+python manage.py process_tasks & 
+gunicorn -w 3 -b 0.0.0.0:8000 core.wsgi --timeout 300
